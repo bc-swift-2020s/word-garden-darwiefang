31c933e09aa72850ed75f7e06301589c487aa43f
Darwie Fang <fangdb@bc.edu>
2020-01-31 15:29:30 -0500

    3.2

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  Word Garden
 //
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    
+    @IBOutlet weak var guessLetterButton: UIButton!
+    
+    
+    @IBOutlet weak var guessCountLabel: UILabel!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("In viewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
     }
-
-
-}
+    func updateUIAfterGuess() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        print("Hey! The guessedLetterFieldChanged!!!")
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        print("In doneKeyPressed, is guessedLetterField the first responder before UpdateUIAfterGuess", guessedLetterField.isFirstResponder)
+       updateUIAfterGuess()
+        print("In doneKeyPressed, is guessedLetterField the first responder after UpdateUIAfterGuess", guessedLetterField.isFirstResponder)
+        
+    }
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+         print("In guessLetterButtonPressed, is guessedLetterField the first responder after UpdateUIAfterGuess", guessedLetterField.isFirstResponder)
+        updateUIAfterGuess()
+         print("In guessLetterButtonPressed, is guessedLetterField the first responder after UpdateUIAfterGuess", guessedLetterField.isFirstResponder) 
+       
+        
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+ }
 
